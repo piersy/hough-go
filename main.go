454f01f850d48c4e52e2d72f551e1c9e9c49f96f
root <root@localhost.localdomain>
2016-06-15 02:02:46 +0100

    First stab,got roughhough transform
    It seems offset somewhat maybe a bit skewed

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"flag"
+	"image"
+	"image/png"
+	_ "image/gif"
+	_ "image/png"
+	_ "image/jpeg"
+	"os"
+	"math"
+)
+
+var (
+	in = flag.String("in", "", "input image")
+	out = flag.String("out", "", "output image")
+)
+
+type cn struct {
+	i float64
+	r float64
+}
+
+func main() {
+	flag.Parse()
+	f, err := os.Open(*in)
+	defer f.Close()
+	if err != nil {
+		println(err)
+		os.Exit(1)
+	}
+	im, _, err := image.Decode(f)
+	//im := image.NewGray(image.Rectangle{image.Point{0, 0}, image.Point{1000, 1000}})
+	hough, _ := hough(im)
+
+	outFile, err := os.Create(*out)
+	defer outFile.Close()
+	if err != nil {
+		println(err)
+		os.Exit(1)
+	}
+	png.Encode(outFile, hough)
+}
+
+func hough(i image.Image) (image.Image ,error){
+
+	var maxRadius float64
+	maxRadius = float64(i.Bounds().Dx() * i.Bounds().Dx() + i.Bounds().Dy() * i.Bounds().Dy())
+	maxRadius = math.Sqrt(float64(maxRadius))
+
+	hough := image.NewGray16(image.Rectangle{image.Point{0, 0}, image.Point{180, int(maxRadius)}})
+		
+	for x:= 0; x<i.Bounds().Dx(); x++{
+		for y:= 0; y<i.Bounds().Dy(); y++{
+			r,_,_,_ := i.At(x,y).RGBA()
+			
+			//black pixel
+			if r == 0 {
+				for t:= 0; t<180; t++{
+					radians := 2.0 * float64(t) * math.Pi /   180.0 
+					r := float64(x) * math.Cos(radians) - float64(y) * math.Sin(radians)
+					g := hough.Gray16At(t, int(r))
+					g.Y += 1000
+					hough.SetGray16(t, int(r), g)
+				}
+			}
+		}
+	}
+	return hough, nil
+	//r = x cos T + y sin T
+}
