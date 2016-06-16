8b0042f69db226ddf2798c4314ea6b23862048a8
root <root@localhost.localdomain>
2016-06-16 01:42:09 +0100

    Next attempt gettign better

--- main.go
+++ main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"flag"
 	"image"
-	"image/png"
 	_ "image/gif"
-	_ "image/png"
 	_ "image/jpeg"
-	"os"
+	"image/png"
+	_ "image/png"
 	"math"
+	"os"
 )
 
 var (
-	in = flag.String("in", "", "input image")
+	in  = flag.String("in", "", "input image")
 	out = flag.String("out", "", "output image")
 )
 
@@ -42,26 +42,26 @@ func main() {
 	png.Encode(outFile, hough)
 }
 
-func hough(i image.Image) (image.Image ,error){
+func hough(i image.Image) (image.Image, error) {
 
 	var maxRadius float64
-	maxRadius = float64(i.Bounds().Dx() * i.Bounds().Dx() + i.Bounds().Dy() * i.Bounds().Dy())
+	maxRadius = float64(i.Bounds().Dx()*i.Bounds().Dx() + i.Bounds().Dy()*i.Bounds().Dy())
 	maxRadius = math.Sqrt(float64(maxRadius))
 
 	hough := image.NewGray16(image.Rectangle{image.Point{0, 0}, image.Point{180, int(maxRadius)}})
-		
-	for x:= 0; x<i.Bounds().Dx(); x++{
-		for y:= 0; y<i.Bounds().Dy(); y++{
-			r,_,_,_ := i.At(x,y).RGBA()
-			
+
+	for x := 0; x < i.Bounds().Dx(); x++ {
+		for y := 0; y < i.Bounds().Dy(); y++ {
+			r, _, _, _ := i.At(x, y).RGBA()
+
 			//black pixel
 			if r == 0 {
-				for t:= 0; t<180; t++{
-					radians := 2.0 * float64(t) * math.Pi /   180.0 
-					r := float64(x) * math.Cos(radians) - float64(y) * math.Sin(radians)
-					g := hough.Gray16At(t, int(r))
-					g.Y += 1000
-					hough.SetGray16(t, int(r), g)
+				for t := 0; t < 180; t++ {
+					radians := -(math.Pi / 2.0) + (float64(t) * math.Pi / 180.0)
+					r := float64(x)*math.Cos(radians) - float64(y)*math.Sin(radians)
+					g := hough.Gray16At(t, int((maxRadius/2.0)-r))
+					g.Y += 300
+					hough.SetGray16(t, int((maxRadius/2.0)-r), g)
 				}
 			}
 		}
