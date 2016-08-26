62e3e8d310f7957a9c60522d3c2dd98467fc5442
piersy <pierspowlesland@gmail.com>
2016-08-26 02:32:55 +0100

    Add benchmarking of normalisation
    The method defined on the Gray16 image is about twice as fast
    as using a util.normaliser.

--- hough/hough_test.go
+++ hough/hough_test.go
@@ -6,8 +6,11 @@ import (
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
+	"math"
 	"os"
 	"testing"
+
+	"github.com/piersy/hough-go/util"
 )
 
 var (
@@ -19,7 +22,40 @@ func init() {
 }
 
 func BenchmarkHough(b *testing.B) {
-	flag.Parse()
+	input := getImage(b)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Hough(input, 400, 400)
+	}
+}
+
+func BenchmarkNormaliseGray16(b *testing.B) {
+	input := getImage(b)
+	gray := Hough(input, 400, 400)
+	pix := gray.Pix
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		normalise(pix, 0, math.MaxUint16/2)
+	}
+}
+
+func normalise(pix []uint16, min, max uint16) {
+	n := util.NewNormaliser(float64(min), float64(max), 0, float64(math.MaxUint16))
+	for i := 0; i < len(pix); i++ {
+		pix[i] = uint16(n.Normalise(float64(pix[i])) + 0.5)
+	}
+}
+
+func BenchmarkNormaliseGray16Method(b *testing.B) {
+	input := getImage(b)
+	gray := Hough(input, 400, 400)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		gray.Normalise()
+	}
+}
+
+func getImage(b *testing.B) image.Image {
 	f, err := os.Open(*in)
 	defer f.Close()
 	if err != nil {
@@ -29,8 +65,5 @@ func BenchmarkHough(b *testing.B) {
 	if err != nil {
 		b.Fatal(err)
 	}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		Hough(input, 400, 400)
-	}
+	return input
 }
